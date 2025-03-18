4513157aa246956eca91fb3dc9db35ba60d9889b
anniedoris <anniecd88@gmail.com>
2025-03-18 18:44:44 -0400

    script for evaluating huggingface llava updated

--- scripts/v1_5/eval/gencad_cadquery_huggingface_llava.sh
+++ scripts/v1_5/eval/gencad_cadquery_huggingface_llava.sh
@@ -6,7 +6,7 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 CHUNKS=${#GPULIST[@]}
 
 CKPT="hf_llava-v1.5-13b"
-SPLIT="cadquery_test_data_subset100"
+SPLIT="external"
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
@@ -16,7 +16,6 @@ for IDX in $(seq 0 $((CHUNKS-1))); do
         --answers-file /orcd/data/faez/001/annie/llava/checkpoints/$CKPT/eval/$SPLIT/${CHUNKS}_${IDX}.jsonl \
         --num-chunks $CHUNKS \
         --chunk-idx $IDX \
-        --temperature 0 \
         --max_new_tokens 1402\
         --conv-mode vicuna_v1 &
 done
